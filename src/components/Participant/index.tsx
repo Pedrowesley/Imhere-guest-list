6f1054ecd67fcffd22bdb2a1e09cadf1ca8635e7
Pedro Wesley <pedro.wesley@osf.digital>
2022-10-21 22:58:02 -0300

    Participant component

---
+++ src/components/Participant/index.tsx
@@ -0,0 +1,20 @@
+import { Text, TouchableOpacity, View } from "react-native";
+
+import { styles } from "./styles";
+
+interface Props {
+  name: string;
+  onRemove?: () => void;
+}
+
+export function Participant(props: Props) {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.name}>{props.name}</Text>
+
+      <TouchableOpacity style={styles.button} onPress={props.onRemove}>
+        <Text style={styles.buttonText}>-</Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
